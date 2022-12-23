4b8468e3027f82e2767d7332268b16b6f5178065
Trabelsi Nosra <83449250+nosra-hub@users.noreply.github.com>
2022-12-23 15:59:28 +0100

    Create AppIntializer.java

---
+++ src/main/java/tn/enig/config/AppIntializer.java
@@ -0,0 +1,11 @@
+
+  package tn.enig.config;
+  
+  import org.springframework.security.web.context.
+  AbstractSecurityWebApplicationInitializer;
+  
+  public class AppInitializer extends
+  AbstractSecurityWebApplicationInitializer{
+  
+  }
+ 
